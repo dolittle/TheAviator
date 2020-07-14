cf580770af10d5961c2e0f575bd3c5a5da6cbea6
Sindre Wilting <sindrewilting@gmail.com>
2020-07-14 12:36:07 +0200

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     extends: "@dolittle/typescript",
     root: true,
     parserOptions: {
-        project: './Sources/*/tsconfig.json',
         sourceType: 'module',
         tsconfigRootDir: __dirname
     }
