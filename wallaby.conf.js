b28ac37d23640ec77cc635ac32803a949500f183
Sindre Wilting <sindrewilting@gmail.com>
2020-07-14 12:22:48 +0200

    Added new configs using new @dolittle/typescript.build

---
+++ wallaby.conf.js
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+const build = require('@dolittle/typescript.build');
+
+module.exports = build.wallaby({
+    setupCallback: (opt) => {
+        require('reflect-metadata');
+    },
+    settingsCallback: (w, settings) => {
+        settings.files = settings.files.map((_) => {
+            _.pattern = _.pattern.replace('@(ts|js)', 'ts');
+            if (_.pattern.startsWith('/')) _.pattern = _.pattern.substr(1);
+            return _;
+        });
+        settings.tests = settings.tests.map((_) => {
+            _.pattern = _.pattern.replace('@(ts|js)', 'ts');
+            if (_.pattern.startsWith('/')) _.pattern = _.pattern.substr(1);
+            return _;
+        });
+    },
+});
