b5448bef7ee4e87f39116294de155195ec40d1a5
Sindre Wilting <sindrewilting@gmail.com>
2020-07-22 15:59:11 +0200

    Make it easier to create scenarios

--- Source/gherkin/MicroserviceScenarioContext.ts
+++ Source/gherkin/MicroserviceScenarioContext.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { ScenarioContext } from '@dolittle/testing.gherkin';
+import { MicroserviceRuleSetContainerBuilder } from '@dolittle/aviator.rules';
 import { MicroserviceInContext, MicroserviceScenarioEnvironment } from './index';
 
 export abstract class MicroserviceScenarioContext extends ScenarioContext {
@@ -11,5 +12,13 @@ export abstract class MicroserviceScenarioContext extends ScenarioContext {
         super.establish(environment);
         Object.entries(environment.microservices)
             .forEach(([microserviceName, microservice]) => this.microservices[microserviceName] = new MicroserviceInContext(microservice));
+    }
+    getRuleSetContainerBuilders(): MicroserviceRuleSetContainerBuilder[] {
+        const builders: MicroserviceRuleSetContainerBuilder[] = [];
+        Object.entries(this.microservices)
+            .forEach(([name, microserviceInContext]) => {
+                builders.push(...microserviceInContext.ruleSetContainerBuilders);
+            });
+        return builders;
     }
 }
