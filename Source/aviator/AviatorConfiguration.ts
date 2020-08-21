383b32ec74c0c35fa88196d3fb056fc8445501a5
Sindre Wilting <sindrewilting@gmail.com>
2020-08-21 20:45:18 +0200

    Fix Aviator

--- Source/aviator/AviatorConfiguration.ts
+++ Source/aviator/AviatorConfiguration.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import banner from './Banner';
-export * from './Aviator';
-export * from './AviatorConfiguration';
-
-export const Banner = banner;
+export type AviatorConfiguration = {
+    namespace: string
+};
