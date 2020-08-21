bc8b27db716dbdae2c6c0cb7f78c7d8387bf1f19
Sindre Wilting <sindrewilting@gmail.com>
2020-08-21 20:19:31 +0200

    Remove ConfgiurationProvider and K8sConfiguration

--- Source/k8s/index.ts
+++ Source/k8s/index.ts
@@ -1,12 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export * from './K8sConfiguration';
 export * from './IRunContext';
 export * from './IRunContexts';
 export * from './IOrchestrator';
 export * from './NamespacedPod';
 export * from './RunContext';
 export * from './RunContexts';
-export * from './ConfigurationProvider';
 export * from './Orchestrator';
